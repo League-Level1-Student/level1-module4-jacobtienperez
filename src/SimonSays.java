c5646c260762efcee128bd739ad4267c1b1c3ab7
perez.jacob <perez.jacob@SEMS-259-30.SEMS259.LAB>
2019-02-13 16:26:24 -0800

    simonsays and chuckle clickre

--- src/SimonSays.java
+++ src/SimonSays.java
@@ -16,6 +16,9 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+
+import com.sun.corba.se.impl.oa.poa.ActiveObjectMap.Key;
+
 import java.io.IOException;
 
 public class SimonSays extends KeyAdapter {
@@ -29,25 +32,50 @@ public class SimonSays extends KeyAdapter {
 	private int tries = 0;
 	private boolean simonSays = false;
 	Date timeAtStart;
+	int points = 0;
 
 	private void makeAlbum() {
 		// 2. Add the four images that match keyboard keys like this: 
 		//images.put(new Integer(KeyEvent.VK_UP), "up.jpg");
+		images.put(new Integer(KeyEvent.VK_UP), "up.jpg");
+		images.put(new Integer(KeyEvent.VK_LEFT), "left.jpg");
+		images.put(new Integer(KeyEvent.VK_RIGHT), "right.jpg");
+		images.put(new Integer(KeyEvent.VK_DOWN), "down.jpg");
+	
 
 		// 3. Use a JOptionPane to tell the user the rules: "Press the matching key when
-		// 'Simon says' otherwise press a different key"
+		// 'Simon says' otherwise press a different key"\
+		JOptionPane.showMessageDialog(null, "press the matching key when simon says otherwise press a different key");
 		
 		// 4. Call the showImage method to show an image
+	
+		showImage();
+		
+	
+		
 	}
 
 	public void keyPressed(KeyEvent e) {
 		// 15. Make a points variable to track the score.
+		int keyCode = e.getKeyCode();
+		
+		tries +=1;
+		
 
 		// 16. If the keyCode matches the imageIndex and "Simon says"
+		if(e.getKeyCode() == imageIndex && simonSays  == true) {
+			points += 1;
 		
+			speak("you got it right");
+		}
+		else if(e.getKeyCode() == imageIndex && simonSays == false) {
+			points -=1;
+			speak("wrong you suck");
+		}
 			// 17. Increase the value of score
 		
 			// 18. Use the speak method to tell the user they were correct
+		
 		
 		// 19. If the keyCode doesn't match the imageIndex and "Simon didn't say..."
 		
@@ -58,41 +86,70 @@ public class SimonSays extends KeyAdapter {
 		// 22. Increment tries by 1
 		
 		// 25. If tries is greater than 9 (or however many you want)...
+		if(tries > 12) { 
+			speak("Your score is " + points);
+			 System.exit(0);
+		}
 		
 			// 26. Tell the user their score
 		
 			// 27. Exit the program
+		    
 
 		// 23. Dispose of the frame
+		frame.dispose();
 
 		// 24. Call the showImage method to show a new image
+		showImage();
 	}
 
 	private void showImage() {
 		// 5. Initialize your frame to a new JFrame()
+		frame = new JFrame();
 		
 		// 6. Set the frame to visible
+		frame.setVisible(true);
 
 		// 7. Uncomment the following line to add a random image to your frame
-		//frame.add(getNextRandomImage());
+		frame.add(getNextRandomImage());
 
 		// 8. Set the name of your frame
+		frame.setName("Image");
 
 		// 9. Pack the frame
+		frame.pack();
 		
 		// 10. Set the defaultCloseOperation of your from to JFrame.EXIT_ON_CLOSE
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		// 11. Add a key listener to the frame
+		frame.addKeyListener(this);
 
 		// 12. Create a new instance of Random
+		Random r = new Random();
 
 		// 13. Use the Random and the speak method to either say 
 		// "Simon says press this key" or "Press this key"
+	
+		simonSays = r.nextBoolean() ;
+		
+		if(simonSays == true) {
+			speak("Simon says press this key");
+		}
+		
+		if(simonSays == false) {
+			speak("quick press this key");
+		}
+	
 
 		// 14. Above, set the value of simonSays to true/false appropriately
+		
+		}
+		
+		
 
 		
-	}
+	
 
 	private Component getNextRandomImage() {
 		this.imageIndex = new Random().nextInt(4) + 37;
@@ -106,11 +163,7 @@ public class SimonSays extends KeyAdapter {
 	}
 
 	void speak(String words) {
-		try {
-			Runtime.getRuntime().exec("say " + words).waitFor();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	System.out.println(words);
 	}
 
 	public static void main(String[] args) throws Exception {
---
+++ src/chuckleclikcer.java
@@ -0,0 +1,12 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class chuckleclikcer {
+public static void main(String[] args) {
+	JFrame frame = new JFrame();
+	JButton butt = new JButton();
+	butt.setText("Joke");
+}
+
+}
+
